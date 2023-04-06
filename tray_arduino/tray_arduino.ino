cf3d3c13e248385d05deafcd6179c2b11b68b463
Samuwhale <sfspithorst@gmail.com>
2023-04-06 12:17:25 +0200

    comments added

--- tray_arduino/tray_arduino.ino
+++ tray_arduino/tray_arduino.ino
@@ -16,18 +16,13 @@
 #define SERIAL Serial
 #endif
 
-//interval at which random values are assigned
-// const unsigned long INTERVAL = 500;
 
-
-
-// array of 10 'random' names
+// array of 10 names to display randomly on startup, normally the username and data would be obtained from reading the NFC chip. This just simulates a random user.
 String realNames[10] = { "Alice", "Bob", "Charlie", "Dave", "Eve", "Frank", "Grace", "Heidi", "Ivan", "Julia" };
-
-static int streakAmount = 5;
-static int meatScore = 1;
-
 String userName = realNames[(int)random(10)];
+
+static int streakAmount = 0; // the amount of days the user has eaten sustainable, yet again, this would normally be obtained from an actual user's NFC chip.
+static int meatScore = 0; // the sustainability of the meal the user is currently eating. This too would normally be set while checking out at the register.
 
 
 void setup() {
@@ -38,17 +33,19 @@ void setup() {
 
   setupMatrix();
 
-  updateMatrix();
+  drawSmiley();
   updateLcd();
-  updateLeds();
+  displayStreaks();
 }
 
 void loop() {
   //  randomizeValues();
 }
 
-// unsigned long randomizeTimer = millis();
+// For evaluation, this function was written to randomize states (random streak, user, meat-score), but in the end it was not used.
 
+// const unsigned long INTERVAL = 500;
+// unsigned long randomizeTimer = millis();
 // void randomizeValues() {
 //   unsigned long currentTime = millis();
 //   if (currentTime - randomizeTimer >= INTERVAL) {
@@ -66,8 +63,9 @@ void loop() {
 //   }
 // }
 
-// LED MATRIX
 
+/* LED MATRIX */
+// The following section covers the functionality of the LED-matrix, in order to display smileys based on the sustainability (meatScore) of the current meal.
 
 
 const uint8_t smile_bmp[] PROGMEM = {
@@ -104,12 +102,8 @@ const uint8_t neutral_bmp[] PROGMEM = {
 };
 
 
-
-
-
-
-
 Adafruit_LEDBackpack matrix = Adafruit_LEDBackpack();
+
 
 
 void setupMatrix() {
@@ -118,7 +112,7 @@ void setupMatrix() {
 }
 
 
-void updateMatrix() {
+void drawSmiley() {
   matrix.clear();
 
   // paint one LED per row.
@@ -146,7 +140,7 @@ void updateMatrix() {
   displaybuffertemp[3] = displaybuffertemp[5];
   displaybuffertemp[5] = tempbuffer2;
 
-  // Mirror data\
+  // Mirror data
   for (uint8_t i = 0; i < 8; i++) {
     uint8_t tempbuffer = displaybuffertemp[i] & 0xFF;
     displaybuffertemp[i] &= 0xFF00;
@@ -164,8 +158,9 @@ void updateMatrix() {
 }
 
 
-// LED STRIP
-// port 7
+/* LED Strip */
+// The following section covers the functionality of the LED-strip, in order to display the user's current streak.
+
 #define LEDS_TO_LIGHT_UP 19
 #define FIRST_SEGMENT_START 2
 #define LED_DATA_PIN 8
@@ -182,7 +177,7 @@ void setupLeds() {
   FastLED.setBrightness(LED_BRIGHTNESS);
 }
 
-void updateLeds() {
+void displayStreaks() {
 
 
   int segmentSize = ceil(static_cast<double>(LEDS_TO_LIGHT_UP) / 5);
@@ -218,59 +213,21 @@ void updateLeds() {
   FastLED.show();
 }
 
-// void updateLeds() {
-//   // calculate the current segment based on the streak amount
-//   int currentSegment = streakAmount / (TOTAL_LED_AMOUNT - FIRST_SEGMENT_START + 1);
 
-//   // loop through each LED
-//   for (int i = 0; i < TOTAL_LED_AMOUNT; i++) {
-//     // calculate the current segment based on the LED index
-//     int segment = (i - FIRST_SEGMENT_START) / ((TOTAL_LED_AMOUNT - FIRST_SEGMENT_START + 1) / 5);
+/* LCD Screen */
+// The following section covers the functionality of the LCD display, to display feedback to the user based on its current meal.
 
-//     // check if this LED should be lit based on the current segment
-//     if (segment < currentSegment) {
-//       // set the LED color based on the segment
-//       switch (segment) {
-//         case 0:
-//           leds[i] = CRGB::Red;
-//           break;
-//         case 1:
-//           leds[i] = CRGB::Blue;
-//           break;
-//         case 2:
-//           leds[i] = CRGB::Yellow;
-//           break;
-//         case 3:
-//           leds[i] = CRGB::Purple;
-//           break;
-//         case 4:
-//           leds[i] = CRGB::Green;
-//           break;
-//       }
-//     } else {
-//       // turn off the LED
-//       leds[i] = CRGB::Black;
-//     }
-//   }
-
-//   // update the LEDs
-//   FastLED.show();
-// }
-
-
-// LCD scherm
-/* 
- * The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
- * 10K resistor:
- * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
- */
+ 
+// The circuit:
+// LCD RS pin to digital pin 12
+// LCD Enable pin to digital pin 11
+// LCD D4 pin to digital pin 5
+// LCD D5 pin to digital pin 4
+// LCD D6 pin to digital pin 3
+// LCD D7 pin to digital pin 2
+// 10K resistor:
+// ends to +5V and ground
+// wiper to LCD VO pin (pin 3
 
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
